a47e0fdb5277d90f83a03db300b934cdc42e3bb4
Ponury <pilarskiprzemyslaw@o2.pl>
2020-11-25 19:57:21 +0100

    Initial commit from Spring Starter

---
+++ BudgetingAssistant/src/main/java/org/ametyst/budgeting/BudgetingAssistantApplication.java
@@ -0,0 +1,13 @@
+package org.ametyst.budgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingAssistantApplication.class, args);
+	}
+
+}
---
+++ BudgetingAssistant/src/test/java/org/ametyst/budgeting/BudgetingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ametyst.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
